f50eaaa7929f7b0106747a603a8bd4409aaec40d
Leonardo Sá <leo@leo-sa.com>
2021-11-27 12:20:45 -0600

    file renames

--- arch/systemd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-cat $SCRIPT_DIR/templates/sleep.conf > /etc/systemd/sleep.conf
-cat $SCRIPT_DIR/templates/logind.conf > /etc/systemd/logind.conf
-
-systemctl restart systemd-logind.service
