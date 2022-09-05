55793ed2be1126022a1fe835b5b6784a88f489f7
Leonardo Sá <leo@leo-sa.com>
2022-09-05 15:08:20 -0500

    wip

---
+++ arch/modules/disk.sh
@@ -0,0 +1,72 @@
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+SWAP_PART_NAME="swap_cesh3"
+MAIN_PART_NAME="arch_linux_1xq2h"
+EFI_PART_NAME="efi_y8jo3"
+
+next_part_number() {
+	partcount=$(sgdisk -p $MAIN_DISK | grep "^\s*[[:digit:]]" | wc -l)
+	echo $(($partcount+1))
+}
+
+get_part_path() {
+	lsblk -l -o PATH,PARTLABEL | grep $1 | awk '{print $1}'
+}
+
+
+create_efi_part() {
+	disk=$1
+	efipart=$(get_part_path $EFI_PART_NAME)
+	if [ ! -z "$efipart" ]; then
+		echo "EFI partition detected. Skipping."
+		return 0
+	fi
+
+	# Create the partition
+	partnum=$(next_part_number)
+	sgdisk -n $partnum:0:+512M -t $partnum:ef00 -c $partnum:"$EFI_PART_NAME" $disk
+	sleep 1
+	mkfs.fat -F 32 $(get_part_path $EFI_PART_NAME)
+}
+
+create_swap_part() {
+	disk=$1
+	if [ ! -z "$(get_part_path $SWAP_PART_NAME)" ]; then
+		echo "Swap partition detected. Skipping."
+		return 0
+	fi
+
+	# Create the partition
+	partnum=$(next_part_number)
+	sgdisk -n $partnum:0:+2G -t $partnum:8200 -c $partnum:"$SWAP_PART_NAME" $disk
+	sleep 1
+	mkswap $(get_part_path $SWAP_PART_NAME)
+}
+
+encrypt_part() {
+	part=$1
+	name=$2
+	cryptsetup luksFormat $part
+	cryptsetup open $part $name
+	mkfs -t ext4 /dev/mapper/$name
+}
+
+create_main_part() {
+	if [ ! -z "$(get_part_path $MAIN_PART_NAME)" ]; then
+		echo "Main partition detected. Skipping."
+		return 0
+	fi
+
+	# Create the partition
+	partnum=$(next_part_number)
+	endsector=$(sgdisk -E $MAIN_DISK)
+	sgdisk -n $partnum:0:$endsector -t $partnum:8300 -c $partnum:"$MAIN_PART_NAME" $MAIN_DISK
+	sleep 1
+
+	MAIN_PART=$(get_part_path "$MAIN_PART_NAME")
+	if [ "$ENCRYPT_MAIN_PART" == "yes" ]; then
+		encrypt_part $MAIN_PART cryptroot
+	else
+		mkfs -t ext4 $MAIN_PART
+	fi
+}
--- arch/setup_partitions.sh
+++ arch/setup_partitions.sh
@@ -7,91 +7,18 @@ set -x
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 source /etc/system_settings
+source $SCRIPT_DIR/modules/disk.sh
 
-next_part_number() {
-	partcount=$(sgdisk -p $MAIN_DISK | grep "^\s*[[:digit:]]" | wc -l)
-	echo $(($partcount+1))
-}
-
-create_gpt_table() {
-	if gdisk -l $MAIN_DISK | grep "GPT: present"; then
-		echo "GPT partition table exists. Skipping."
-		return
-	fi
-	# TODO
-}
 
 # -----------------------------------------------------------------------------
 # Create partitions. First is EFI, then 2G swap space, and then fill the
 # rest of the disk with a linux partition.
 # -----------------------------------------------------------------------------
-create_boot_part() {
-	if [ "$USE_EFI_PART" != "yes" ]; then
-		echo "Skipping creation of EFI boot partition."
-		return
-	fi
-	efipart=$(fdisk -l $MAIN_DISK | grep EFI | awk '{print $1}')
-	if [ ! -z "$efipart" ]; then
-		echo "EFI partition detected. Skipping."
-		return 0
-	fi
 
-	# Create the partition
-	partnum=$(next_part_number)
-	sgdisk -n $partnum:0:+512M -t $partnum:ef00 -c $partnum:"EFI boot" $MAIN_DISK
-	sleep 1
-
-	efipart=$(fdisk -l $MAIN_DISK | grep EFI | awk '{print $1}')
-	mkfs.fat -F 32 $efipart
-}
-
-create_swap_part() {
-	if [ "$USE_SWAP_PART" != "yes" ]; then
-		echo "Skipping creation of swap partition."
-		return
-	fi
-	swappart=$(fdisk -l $MAIN_DISK | grep swap | awk '{print $1}')
-	if [ ! -z "$swappart" ]; then
-		echo "Swap partition detected. Skipping."
-		return 0
-	fi
-
-	# Create the partition
-	partnum=$(next_part_number)
-	sgdisk -n $partnum:0:+2G -t $partnum:8200 -c $partnum:"swap" $MAIN_DISK
-	sleep 1
-
-	swappart=$(fdisk -l $MAIN_DISK | grep swap | awk '{print $1}')
-	mkswap $swappart
-}
-
-encrypt_main_part() {
-	cryptsetup luksFormat $MAIN_PART
-	cryptsetup open $MAIN_PART cryptroot
-	mkfs -t ext4 /dev/mapper/cryptroot
-}
-
-create_main_part() {
-	MAIN_PART_BLOCK=$(lsblk -ln -o NAME,PARTTYPE $MAIN_DISK | grep 0x83 | awk '{print $1}')
-	if [ ! -z "$MAIN_PART_BLOCK" ]; then
-		echo "Main partition detected. Skipping."
-		return 0
-	fi
-
-	# Create the partition
-	partnum=$(next_part_number)
-	endsector=$(sgdisk -E $MAIN_DISK)
-	sgdisk -n $partnum:0:$endsector -t $partnum:8300 -c $partnum:"arch linux" $MAIN_DISK
-	sleep 1
-
-	MAIN_PART=/dev/$(lsblk -ln -o NAME,PARTTYPE $MAIN_DISK | grep 0x83 | awk '{print $1}')
-	if [ "$ENCRYPT_MAIN_PART" == "yes" ]; then
-		encrypt_main_part
-	else
-		mkfs -t ext4 $MAIN_PART
-	fi
-}
-
-create_boot_part
-create_swap_part
-create_main_part
+if [ "$USE_EFI_PART" = "yes" ]; then
+	create_efi_part $MAIN_DISK
+fi
+if [ "$USE_SWAP_PART" = "yes" ]; then
+	create_swap_part $MAIN_DISK
+fi
+create_main_part $MAIN_DISK
