538b27504bebe7630e19474d2e375fa236e76615
Leonardo Sá <leo@leo-sa.com>
2022-09-04 19:29:31 -0500

    wip

--- arch/install_from_scratch.sh
+++ arch/install_from_scratch.sh
@@ -41,7 +41,7 @@ source /etc/system_settings
 
 # Temporarily make the settings available to chroot
 # Will be made permanent later on.
-/#ln -s /etc/system_settings /mnt/etc/system_settings
+#ln -s /etc/system_settings /mnt/etc/system_settings
 
 # Set hostname
 #echo "$MACHINE_NAME" > /mnt/etc/hostname
