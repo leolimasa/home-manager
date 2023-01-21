2c216fc18a8133e71da23fa21006a485862afd8a
Leonardo Sá <leo@leo-sa.com>
2023-01-21 13:56:49 -0600

    lua yaml

--- files/init.lua
+++ files/init.lua
@@ -230,7 +230,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'sql', 'nix', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'sql', 'nix', 'yaml', 'json', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
