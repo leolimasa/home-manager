11f5e7544dea5f3062f43eb1cb87d0cc569b6728
Leonardo Sá <leo@transportant.us>
2023-01-25 09:30:21 -0600

    added ssh_rc

--- vm-leo-tsp.nix
+++ vm-leo-tsp.nix
@@ -9,6 +9,7 @@
 
   config = {
     home.file.".ssh/config".text = builtins.readFile /home/leo/pr/tsp/git/notes/tsp_ssh_config;
+    home.file.".ssh/rc".text = builtins.readFile ./files/ssh_rc;
   };
 
 }
